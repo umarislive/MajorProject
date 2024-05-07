a9a9a34fd90a65259dfc8107b344fdf2c8a1c2ad
Umar Farooq <umarisonline007@gmail.com>
2024-05-07 23:08:15 +0500

    Initial commit

---
+++ models/listing.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: String,
+    image: {
+        type: String,
+        default: "https://unsplash.com/photos/birds-eye-view-photograph-of-green-mountains-01_igFr7hd4",
+        set: (v) => v === "" ? "https://unsplash.com/photos/birds-eye-view-photograph-of-green-mountains-01_igFr7hd4" : v,
+    },
+    price: Number,
+    location: String,
+    country: String
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
